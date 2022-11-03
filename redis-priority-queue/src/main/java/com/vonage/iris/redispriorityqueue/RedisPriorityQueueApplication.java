82d3d3469c6558a9a93717df548b24068a59f627
ashishhg <ashish.hg@vonage.com>
2022-11-03 13:27:46 +0530

    Initial checkin

---
+++ redis-priority-queue/src/main/java/com/vonage/iris/redispriorityqueue/RedisPriorityQueueApplication.java
@@ -0,0 +1,13 @@
+package com.vonage.iris.redispriorityqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPriorityQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisPriorityQueueApplication.class, args);
+	}
+
+}
---
+++ redis-priority-queue/src/test/java/com/vonage/iris/redispriorityqueue/RedisPriorityQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vonage.iris.redispriorityqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisPriorityQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
